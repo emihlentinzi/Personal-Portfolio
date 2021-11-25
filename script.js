f2ebe080b18b8db5a4fcd3556cf0f80dc2ec731b
emihlentinzi <76787646+emihlentinzi@users.noreply.github.com>
2021-11-25 14:19:48 +0200

    Add files via upload

--- script.js
+++ script.js
@@ -1,3 +1,4 @@
+
 $(document).ready(function(){
     $(window).scroll(function(){
       
@@ -14,6 +15,22 @@ $(document).ready(function(){
         }else{
             $('.scroll-up-btn').removeClass("show");
         }
+    });
+    
+    // slide-up script
+    $('.scroll-up-btn').click(function(){
+        $('html').animate({scrollTop: 0});
+        // removing smooth scroll on slide-up button click
+        $('html').css("scrollBehavior", "auto");
+    });
+    $('.navbar .menu li a').click(function(){
+        // applying again smooth scroll on menu items click
+        $('html').css("scrollBehavior", "smooth");
+    });
+    // toggle menu/navbar script
+    $('.menu-btn').click(function(){
+        $('.navbar .menu').toggleClass("active");
+        $('.menu-btn i').toggleClass("active");
     });
 
     // typing text animation script
